e764096b2da86a555c82732e237e481a3f898124
jonycorn <jonas.vybiral.jv@gmail.com>
2022-01-18 20:48:34 +0100

    changed gsp

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,8 @@ import SideBar from "../components/SideBar";
 import { Room } from "./../types/rooms";
 
 export default function App({ rooms }: { rooms: { data: Room[] } }) {
+  console.log("HEEEEEEEELP" + process.env.NODE_ENV);
+  console.log("HEEEEEEEELP" + process.env.VERCEL_URL);
   return (
     <div className="flex">
       <SideBar />
@@ -15,7 +17,17 @@ export async function getStaticProps() {
   console.log("HEEEEEEEELP" + process.env.NODE_ENV);
   console.log("HEEEEEEEELP" + process.env.VERCEL_URL);
 
-  const res = await fetch("http://localhost:3000/api/rooms");
+  if (process.env.NODE_ENV === "development") {
+    const res = await fetch("http://localhost:3000/api/rooms");
+    const data = await res.json();
+    return {
+      props: {
+        rooms: data,
+      },
+    };
+  }
+
+  const res = await fetch(`${process.env.VERCEL_URL}/api/rooms`);
   const data = await res.json();
 
   return {
